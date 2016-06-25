2e2812a6a90f24c561f706ffed6dbc08037b3231
Imam <imam.tauhid.dar@icloud.com>
2016-06-25 18:03:39 +0700

    up

--- migrations/20160526190004_stream.js
+++ migrations/20160526190004_stream.js
@@ -2,10 +2,9 @@
 exports.up = function(knex, Promise) {
 	return knex.schema.createTable('stream', (table) => {
 		table.uuid('id').primary()
-		table.string('username').references('user.username')
+		table.string('user_id').references('user.id')
 		table.string('content')
 		table.integer('vote')
-		table.string('stream_id').references('stream.id')
 		table.dateTime('datecreated')
 	})
 };
